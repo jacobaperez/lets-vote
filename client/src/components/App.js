4b0423ae1afc22db418faa34dcde358496e0de43
Jake Perez <jacobanthony.perez@gmail.com>
2017-11-21 15:06:30 -0800

    Setting up app skeleton

---
+++ client/src/components/App.js
@@ -0,0 +1,1 @@
+// Main app goes here!
---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+// this is the main app. connects to the dom.
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+import App from './component/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
